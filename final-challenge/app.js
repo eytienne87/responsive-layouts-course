025390064d7ea5c4aa379d96652d4304d21b1c03
Etienne Tessier <etienne_tessier100@hotmail.ca>
2022-01-11 09:28:28 -0500

    added my footer for mobile only

--- final-challenge/app.js
+++ final-challenge/app.js
@@ -2,5 +2,5 @@ const hamburgerButton = document.querySelector('.nav-toggle');
 const navbar = document.querySelector('nav')
 
 hamburgerButton.addEventListener('click', () => {
-  console.log(navbar.classList.toggle('nav-visible'));
+  navbar.classList.toggle('nav-visible')
 });
